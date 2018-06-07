62876e7bc4ebd5425f0748130c10d99f8521e4ef
Thepahka <scifuentesarango@misena.edu.co>
2018-06-07 16:15:37 -0500

    nose

--- views/modules/admin/index.php
+++ views/modules/admin/index.php
@@ -1,7 +1,6 @@
-<div class="">
+<div class="content">
   <a href="#"> <li class=""> <i class="fa fa-user"></i>Aprendiz </li> </a>
   <a href="#"> <li class=""> <i class="fa fa-book"></i>lectiva </li> </a>
   <a href="#"> <li class=""> <i class="fa fa-building"></i>Practica </li> </a>
   <a href="#"> <li class=""> <i class="fa fa-address-book"></i>Docentes </li> </a>
-
 </div>
