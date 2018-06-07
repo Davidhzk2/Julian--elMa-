89607da99014d04afbdec08243be7cefd26be603
Thepahka <scifuentesarango@misena.edu.co>
2018-06-07 13:25:43 -0500

    mire

--- views/include/admin/scope.header.php
+++ views/include/admin/scope.header.php
@@ -2,13 +2,14 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="views/assets/css/master.css">
+    <link rel="stylesheet" href="views/assets/css/master-admin.css">
     <link rel="shortcut icon" href="views/assets/img/excel.ico">
     <link href="https://fonts.googleapis.com/css?family=Cabin:400,700|Roboto:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
     <title>Admin elma </title>
   </head>
   <body>
     <section>
       <header>
-        <h1>Julian elma</h1>
+        <?php require_once "views/modules/admin/menutop.php"; ?>
       </header>
---
+++ views/modules/admin/menutop.php
@@ -0,0 +1,13 @@
+<nav>
+  <ul>
+    <div class="wrapmenu">
+      <div class="">
+        <i class="fa fa-bars"></i>
+      </div>
+      <div class="">
+
+      </div>
+
+    </div>
+  </ul>
+</nav>
