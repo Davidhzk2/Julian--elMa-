68d08d42562527cb0042a0eb7d5670e0b9fada53
Thepahka <scifuentesarango@misena.edu.co>
2018-06-07 15:03:58 -0500

    resgistrar

---
+++ views/modules/admin/registrar.php
@@ -0,0 +1,22 @@
+<div class="wrapRegis">
+   <h1>Registrar Docente</h1>
+   <form class="" action="" method="post">
+     <div class="form-group">
+       <label for="">Nombre:</label>
+       <input type="text" class="k" name="" value="">
+     </div>
+     <div class="form-group">
+       <label for="">Apellido</label>
+       <input type="text" class="k" name="" value="">
+     </div>
+     <div class="form-group">
+       <label for="">Ficha</label>
+       <input type="text" class="k" name="" value="">
+     </div>
+     <div class="form-group">
+       <label for="">Programa</label>
+       <input type="text" class="k" name="" value="">
+     </div>
+     <button type="submit" name="button">Registrar</button>
+   </form>
+</div>
