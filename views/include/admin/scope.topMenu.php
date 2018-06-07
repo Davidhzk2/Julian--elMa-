608fb9d6c1c17e88bcaf93030e956431db412ef2
Thepahka <scifuentesarango@misena.edu.co>
2018-06-07 15:06:13 -0500

    top

--- views/include/admin/scope.topMenu.php
+++ views/include/admin/scope.topMenu.php
@@ -1,16 +1,17 @@
 <nav class="topMenu">
-    <ul>
-      <div class="wrap">
-        <div class="wrapitem">
-          <li class="item"><i class="fa fa-bars"></i></li>
-            <form class="" action="index.html" method="post">
-              <input type="text"placeholder="Ingrese ficha" name="" value="">
-              <button type="button" name="button">Buscar</button>
-          </form>
-        </div>
-        <div class="wrap-out">
-          <a href="#"><li class="item"><i class="fa fa-sign-out-alt"></i></li></a>
-        </div>
-    </div>
-    </ul>
+   <ul>
+     <div class="wrap">
+       <div class="wrapitem">
+         <li class="item"><i class="fa fa-bars"></i></li>
+           <form class="" action="index.html" method="post">
+             <input type="text"placeholder="Ingrese ficha" name="" value="">
+             <button type="button" name="button">Buscar</button>
+         </form>
+       </div>
+       <div class="wrap-out">
+         <a href="registrar"> <li> <i class="fa fa-user-plus"></i> </li> </a>
+         <a href="#"><li class="item"><i class="fa fa-sign-out-alt"></i></li></a>
+       </div>
+   </div>
+   </ul>
 </nav>
