326c1ebc7bc874f50a248d92e652c855359eef0b
julian <jdgiraldo08@misena.edu.co>
2018-06-07 14:44:15 -0500

    wsedthj

--- views/include/docente/scope.header.php
+++ views/include/docente/scope.header.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="views/assets/css/master.css">
+    <link rel="stylesheet" href="views/assets/css/master-docente.css">
     <link rel="shortcut icon" href="views/assets/img/excel.ico">
     <link href="https://fonts.googleapis.com/css?family=Cabin:400,700|Roboto:400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
@@ -11,5 +11,6 @@
   <body>
     <section>
       <header>
-        <?php require_once "views/modules/docente/menutop.php"; ?>
+        <?php require_once "views/include/docente/scope.topMenu.php"; ?>
+        <?php require_once "views/include/docente/scope.leftMenu.php"; ?>
       </header>
---
+++ views/include/docente/scope.leftMenu.php
@@ -0,0 +1,7 @@
+<nav class="wrapLeftMenu">
+  <ul class="leftMenu">
+    <a href="#"><li class="itemLeft"><i class="fas fa-user"></i> info. aprendiz</li></a>
+    <a href="#"><li class="itemLeft"><i class="fas fa-book"></i> info. lectiva</li></a>
+    <a href="#"><li class="itemLeft"><i class="fas fa-building"></i> practicas</li></a>
+  </ul>
+</nav>
